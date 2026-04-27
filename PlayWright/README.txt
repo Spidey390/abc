6df9085b9e1952695f8ea300dc94d7280f5c89c4
Spidey390 <hariprasathv52@gmail.com>
2026-04-28 04:22:02 +0530

    Delete PlayWright directory

--- PlayWright/README.txt
+++
@@ -1,5 +0,0 @@
-npm init -y
-npm install -D @playwright/test
-npx playwright install
-npx playwright test
-npx playwright show-report
--- PlayWright/playwright.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require("@playwright/test");
-
-module.exports = defineConfig({
-  testDir: "./tests",
-  reporter: "html",
-});
